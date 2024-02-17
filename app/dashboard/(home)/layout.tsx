2ba70de9f20298eb5f5a52e856e1cc289d1a6632
srikar kudurmalla <srikar2985969c@gmail.com>
2024-02-17 23:06:24 +0530

    Posts

---
+++ app/dashboard/(home)/layout.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Headear'
+import React from 'react'
+
+function HomePageLayout({children}:{children: React.ReactNode}) {
+  return (
+    <div>
+     <Header/> 
+    </div>
+  )
+}
+
+export default HomePageLayout
