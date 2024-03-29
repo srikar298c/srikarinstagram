740af9f7d1b0cad9e943d146ac4a817c402390f2
srikar kudurmalla <srikar2985969c@gmail.com>
2024-03-29 12:06:36 +0530

    skeltons and suspense

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -1,14 +1,14 @@
 import Posts from "@/components/Posts";
-
-
+import { PostsSkeleton } from "@/components/Skeltons";
 import { Suspense } from "react";
 
 function DashboardPage() {
   return (
     <main className="flex w-full flex-grow">
       <div className="flex flex-col flex-1 gap-y-8 max-w-lg mx-auto pb-20">
-      <Posts/>
- 
+        <Suspense fallback={<PostsSkeleton />}>
+          <Posts />
+        </Suspense>
       </div>
     </main>
   );
