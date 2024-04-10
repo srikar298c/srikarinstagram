c5703b6fb3d65c22a1871f1cc52551fc7c247200
srikar kudurmalla <srikar2985969c@gmail.com>
2024-04-10 19:15:28 +0530

    viewpost in postview

--- components/ViewPost.tsx
+++ components/ViewPost.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
 
-function ViewPost() {
-  return (
-    <div>ViewPost</div>
-  )
+function ViewPost({ className }: { className?: string }) {
+    return (
+        <div className={cn("flex p-3", className)}>
+            <button
+                onClick={() => window.location.reload()}
+                className="text-sky-500 hover:text-sky-700 font-semibold text-sm"
+            >
+                View post
+            </button>
+        </div>
+    );
 }
 
-export default ViewPost
+export default ViewPost;
