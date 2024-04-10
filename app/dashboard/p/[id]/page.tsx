454c72412269ff5a147ecd6daafb027e3cce546a
srikar kudurmalla <srikar2985969c@gmail.com>
2024-04-10 22:26:48 +0530

    Skeltons in routing

--- app/dashboard/p/[id]/page.tsx
+++ app/dashboard/p/[id]/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { SinglePostSkeleton } from "@/components/Skeletons";
+import { Suspense } from "react";
+import { Separator } from "@/components/ui/separator";
+import SinglePost from "@/components/SinglePost";
+import MorePosts from "@/components/MorePosts";
 
-export default function PostPage() {
+function PostPage({ params: { id } }: { params: { id: string } }) {
   return (
-    <div></div>
-  )
+    <div>
+      <Suspense fallback={<SinglePostSkeleton />}>
+        <SinglePost id={id} />
+      </Suspense>
+
+      <Separator className="my-12 max-w-3xl lg:max-w-4xl mx-auto" />
+
+      <Suspense>
+        <MorePosts postId={id} />
+      </Suspense>
+    </div>
+  );
 }
+
+export default PostPage;
