d7b2026efe4555bdb97b96cb145548f2a053677c
srikar kudurmalla <srikar2985969c@gmail.com>
2024-04-10 18:26:49 +0530

    DB opeartions for comments

--- lib/data.ts
+++ lib/data.ts
@@ -36,81 +36,49 @@ export async function fetchPosts() {
   }
 }
 
-// export async function fetchPostById(id: string) {
-//   noStore();
 
-//   try {
-//     const data = await prisma.post.findUnique({
-//       where: {
-//         id,
-//       },
-//       include: {
-//         comments: {
-//           include: {
-//             user: true,
-//           },
-//           orderBy: {
-//             createdAt: "desc",
-//           },
-//         },
-//         likes: {
-//           include: {
-//             user: true,
-//           },
-//         },
-//         savedBy: true,
-//         user: true,
-//       },
-//     });
 
-//     return data;
-//   } catch (error) {
-//     console.error("Database Error:", error);
-//     throw new Error("Failed to fetch post");
-//   }
-// }
+export async function fetchPostsByUsername(username: string, postId?: string) {
+  noStore();
 
-// export async function fetchPostsByUsername(username: string, postId?: string) {
-//   noStore();
+  try {
+    const data = await prisma.post.findMany({
+      where: {
+        user: {
+          username,
+        },
+        NOT: {
+          id: postId,
+        },
+      },
+      include: {
+        comments: {
+          include: {
+            user: true,
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+        likes: {
+          include: {
+            user: true,
+          },
+        },
+        savedBy: true,
+        user: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
 
-//   try {
-//     const data = await prisma.post.findMany({
-//       where: {
-//         user: {
-//           username,
-//         },
-//         NOT: {
-//           id: postId,
-//         },
-//       },
-//       include: {
-//         comments: {
-//           include: {
-//             user: true,
-//           },
-//           orderBy: {
-//             createdAt: "desc",
-//           },
-//         },
-//         likes: {
-//           include: {
-//             user: true,
-//           },
-//         },
-//         savedBy: true,
-//         user: true,
-//       },
-//       orderBy: {
-//         createdAt: "desc",
-//       },
-//     });
-
-//     return data;
-//   } catch (error) {
-//     console.error("Database Error:", error);
-//     throw new Error("Failed to fetch posts");
-//   }
-// }
+    return data;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch posts");
+  }
+}
 
 // export async function fetchProfile(username: string) {
 //   noStore();
